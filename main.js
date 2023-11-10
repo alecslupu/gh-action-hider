8b136806f623d85cc0caf1d94e439921852c1566
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-11-10 09:16:10 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const hideSuccessfulActions = function(){
+  let indicatorContainer = document.querySelectorAll(".merge-status-list");
+
+  if(indicatorContainer.length == 2) {
+    indicatorContainer[1].querySelectorAll(".octicon-check").forEach((e) => { 
+      e.parentElement.parentElement.style.display = "none"; 
+      e.parentElement.parentElement.classList.remove("d-flex");
+    });
+  }
+}
+if ( window.location.href.match( /https?:\/\/github\.com\/\w+\/\w+\/pull\/\d+/) ) {
+  window.setInterval(hideSuccessfulActions, 100);
+}
